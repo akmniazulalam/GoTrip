12c9f8a7fd230218e6c43f4f88aa3ad44e7f18ae
akmniazulalam <niazulalam097@gmail.com>
2026-06-04 13:20:35 +0600

    newsletter add

--- app/(components)/Footer.jsx
+++ app/(components)/Footer.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import Container from "./Container";
+import Flex from "./Flex";
+import newsletter from "../../public/newsletter 1.svg";
+import Image from "next/image";
 
 const Footer = () => {
   return (
-    <div className='py-10 bg-amber-800'>Footer</div>
-  )
-}
+    <section className="py-15 bg-[#0D2857]">
+      <Container>
+        <div className="flex justify-between items-center">
+          <Flex className={"gap-6"}>
+            <Image src={newsletter} alt="newsletter" height={60} width={60} />
+            <div className="">
+              <h4 className="font-jost text-white text-[26px] font-semibold">
+                Your Travel Journey Starts Here
+              </h4>
+              <p className="font-jost text-base leading-7.5 text-white">
+                Sign up and we'll send the best deals to you
+              </p>
+            </div>
+          </Flex>
+          <Flex className={"gap-x-2.5"}>
+            <input type="email" placeholder="Your Email" className="w-102.5 h-15 py-4 px-5 rounded-sm font-jost text-base bg-white text-black focus:outline-0" />
+            <button className="py-3.5 px-7.5 rounded-sm text-base font-jost font-medium cursor-pointer h-15 bg-[#3554D1] text-white">Subscribe</button>
+          </Flex>
+        </div>
+      </Container>
+    </section>
+  );
+};
 
-export default Footer
+export default Footer;
--- app/page.js
+++ app/page.js
@@ -1,20 +1,41 @@
 import Image from "next/image";
-import iphone from "../public/iphone-16-pro-max-256gb-black-titanium.jpeg"
+import { RxArrowTopRight } from "react-icons/rx";
 import Container from "./(components)/Container";
+import Flex from "./(components)/Flex";
 
 export default function Home() {
   return (
     <>
-    <secion className="bg-[url(../public/bg.webp)] bg-no-repeat bg-cover bg-center">
-      <div className="pt-64 pb-52.5 bg-linear-[270deg] from-[rgba(5,16,54,0.5)] to-[#051036] opacity-85">
-        <Container>
-        <div className="text-center">
-          <h1 className="font-semibold font-jost text-6xl text-white">Find Next Place To Visit</h1>
-          <p className="font-jost text-white font-medium text-base leading-7.5 pt-6">Discover amzaing places at exclusive deals</p>
+      <secion className="bg-[url(../public/bg.webp)] bg-no-repeat bg-cover bg-center">
+        <div className="pt-64 pb-52.5 bg-linear-[270deg] from-[rgba(5,16,54,0.5)] to-[#051036] opacity-85">
+          <Container>
+            <div className="text-center">
+              <h1 className="font-semibold font-jost text-6xl text-white">
+                Find Next Place To Visit
+              </h1>
+              <p className="font-jost text-white font-medium text-base leading-7.5 pt-6">
+                Discover amzaing places at exclusive deals
+              </p>
+            </div>
+          </Container>
         </div>
-      </Container>
-      </div>
-    </secion>
+      </secion>
+
+      <section className="pt-30 pb-15">
+        <Container>
+          <Flex>
+            <div>
+              <h3 className="font-jost text-3xl text-[#051036] font-semibold">
+                Popular Destinations
+              </h3>
+              <p className="font-jost text-[#697488] text-base leading-7.5 mt-2">
+                These popular destinations have a lot to offer
+              </p>
+            </div>
+            <button className="py-3.5 px-7 bg-[#3554d10d] rounded-sm font-jost font-medium text-base transition-all duration-300 ease-[cubic-bezier(0.165, 0.84, 0.44, 1)] text-[#3554D1] cursor-pointer hover:bg-[#3554D1] hover:text-white">View All Destinations <RxArrowTopRight className={"ml-1 text-2xl inline"}/></button>
+          </Flex>
+        </Container>
+      </section>
     </>
   );
 }
