6831cb8fd90a658beae73dd9f18ecd356ca4ea42
akmniazulalam <niazulalam097@gmail.com>
2026-06-10 23:39:03 +0600

    Blog images add

--- app/page.js
+++ app/page.js
@@ -9,7 +9,11 @@ import featureOne from "../public/1.svg";
 import featureTwo from "../public/2.svg";
 import featureThree from "../public/3.svg";
 import reviewer from "../public/1 (1).webp";
+import BlogOne from "../public/1 (2).webp";
+import BlogTwo from "../public/2 (1).webp";
+import BlogThree from "../public/3,webp";
 import { FaStar } from "react-icons/fa";
+import Link from "next/link";
 
 export default function Home() {
   const [activeTab, setActiveTab] = useState("Hotel");
@@ -190,22 +194,36 @@ export default function Home() {
         <Container>
           <div className="flex justify-between">
             <div className="py-5 px-4 w-[40%]">
-              <h2 className="font-jost font-semibold text-3xl text-primaryText w-[70%] leading-11">What our customers are saying us?</h2>
-              <p className="text-base leading-7.5 text-[#697488] mt-5 font-jost">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas varius tortor nibh, sit amet tempor nibh finibus et. Aenean eu enim justo.</p>
+              <h2 className="font-jost font-semibold text-3xl text-primaryText w-[70%] leading-11">
+                What our customers are saying us?
+              </h2>
+              <p className="text-base leading-7.5 text-[#697488] mt-5 font-jost">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Maecenas varius tortor nibh, sit amet tempor nibh finibus et.
+                Aenean eu enim justo.
+              </p>
               <div className="pt-15 grid grid-cols-[42%_40%]">
                 <div>
-                  <p className="text-3xl font-semibold font-jost text-primaryText">13m+</p>
-                  <span className="font-jost text-base leading-7.5 text-[#697488]">Happy People</span>
+                  <p className="text-3xl font-semibold font-jost text-primaryText">
+                    13m+
+                  </p>
+                  <span className="font-jost text-base leading-7.5 text-[#697488]">
+                    Happy People
+                  </span>
                 </div>
                 <div>
-                  <p className="text-3xl font-semibold font-jost text-primaryText">4.88</p>
-                  <span className="font-jost text-base leading-7.5 text-[#697488]">Overall rating</span>
+                  <p className="text-3xl font-semibold font-jost text-primaryText">
+                    4.88
+                  </p>
+                  <span className="font-jost text-base leading-7.5 text-[#697488]">
+                    Overall rating
+                  </span>
                   <div className={"flex gap-x-1 items-center mt-1.5"}>
-                    <FaStar className="text-hoverText text-[10px]"/>
-                    <FaStar className="text-hoverText text-[10px]"/>
-                    <FaStar className="text-hoverText text-[10px]"/>
-                    <FaStar className="text-hoverText text-[10px]"/>
-                    <FaStar className="text-hoverText text-[10px]"/>
+                    <FaStar className="text-hoverText text-[10px]" />
+                    <FaStar className="text-hoverText text-[10px]" />
+                    <FaStar className="text-hoverText text-[10px]" />
+                    <FaStar className="text-hoverText text-[10px]" />
+                    <FaStar className="text-hoverText text-[10px]" />
                   </div>
                 </div>
               </div>
@@ -214,24 +232,48 @@ export default function Home() {
               <div className="flex gap-x-7.5 items-center">
                 <Image src={reviewer} alt="reviewer" height={80} width={80} />
                 <div>
-                  <p className="text-base font-medium font-jost text-primaryText leading-6.5">Annette Black</p>
-                  <span className="text-[15px] text-[#697488] font-jost">UX / UI Designer</span>
+                  <p className="text-base font-medium font-jost text-primaryText leading-6.5">
+                    Annette Black
+                  </p>
+                  <span className="text-[15px] text-[#697488] font-jost">
+                    UX / UI Designer
+                  </span>
                 </div>
               </div>
-              <p className="mt-8 w-[94%] font-jost font-medium text-[18px] text-primaryText leading-7.5">The place is in a great location in Gumbet. The area is safe and beautiful. The apartment was comfortable and the host was kind and responsive to our requests.</p>
+              <p className="mt-8 w-[94%] font-jost font-medium text-[18px] text-primaryText leading-7.5">
+                The place is in a great location in Gumbet. The area is safe and
+                beautiful. The apartment was comfortable and the host was kind
+                and responsive to our requests.
+              </p>
             </div>
           </div>
         </Container>
       </section>
 
       <section className="pt-30 pb-15">
-        <div className="text-center">
-          <h2 className="font-jost font-semibold text-3xl text-primaryText leading-11">Get inspiration for your next trip</h2>
-          <p className="text-base leading-7.5 text-[#697488] mt-1 font-jost">Interdum et malesuada fames</p>
-          <div className="pt-10 grid grid-cols-3 gap-7.5">
-            
+        <Container>
+          <div className="text-center">
+            <h2 className="font-jost font-semibold text-3xl text-primaryText leading-11">
+              Get inspiration for your next trip
+            </h2>
+            <p className="text-base leading-7.5 text-[#697488] mt-1 font-jost">
+              Interdum et malesuada fames
+            </p>
+            <div className="pt-10 grid grid-cols-3 gap-7.5">
+              <Link href={"/"}>
+                <div>
+                  <Image
+                    src={BlogOne}
+                    alt="BlogOne"
+                    height={308}
+                    width={410}
+                    className="rounded-lg"
+                  />
+                </div>
+              </Link>
+            </div>
           </div>
-        </div>
+        </Container>
       </section>
     </>
   );
