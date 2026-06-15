34e755f07f96af73e9045e76fc932a314a346695
akmniazulalam <niazulalam097@gmail.com>
2026-06-16 01:09:08 +0600

    disabled arrow color change

--- app/(components)/RecommendNext.jsx
+++ app/(components)/RecommendNext.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import { LiaArrowRightSolid } from 'react-icons/lia'
 
-const RecommendNext = ({onClick}) => {
+const RecommendNext = ({onClick, className = ""}) => {
+  const isDisabled = className.includes("slick-disabled");
+
   return (
-    <button onClick={onClick} className='absolute -bottom-17.5 right-[43%] cursor-pointer z-50'><LiaArrowRightSolid className='text-primaryText text-2xl hover:text-[#7E53F9] hover:translate-x-1.5 hover:scale-125 transition-all duration-300 ease-[cubic-bezier(0.215, 0.61, 0.355, 1)] z-50' /></button>
+    <button
+      onClick={isDisabled ? undefined : onClick}
+      className={`absolute -bottom-17.5 right-[43%] z-50 ${isDisabled ? "cursor-default" : "cursor-pointer"}`}>
+      <LiaArrowRightSolid
+        className={`text-2xl transition-all duration-300 ease-[cubic-bezier(0.215, 0.61, 0.355, 1)] z-50 ${isDisabled ? "text-[#1010104d]" : "text-primaryText hover:text-[#7E53F9] hover:translate-x-1.5 hover:scale-125"}`}
+      />
+    </button>
   )
 }
 
--- app/(components)/RecommendPrev.jsx
+++ app/(components)/RecommendPrev.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import { LiaArrowLeftSolid } from 'react-icons/lia'
 
-const RecommendPrev = ({onClick}) => {
+const RecommendPrev = ({onClick, className = ""}) => {
+  const isDisabled = className.includes("slick-disabled");
+
   return (
-    <button onClick={onClick} className='cursor-pointer absolute -bottom-17.5 left-[43%] z-50'><LiaArrowLeftSolid className='text-primaryText text-2xl hover:text-[#7E53F9] hover:-translate-x-1.5 hover:scale-125 transition-all duration-300 ease-[cubic-bezier(0.215, 0.61, 0.355, 1)]' /></button>
+    <button
+      onClick={isDisabled ? undefined : onClick}
+      className={`absolute -bottom-17.5 left-[43%] z-50 ${isDisabled ? "cursor-default" : "cursor-pointer"}`}>
+      <LiaArrowLeftSolid
+        className={`text-2xl transition-all duration-300 ease-[cubic-bezier(0.215, 0.61, 0.355, 1)] ${isDisabled ? "text-[#1010104d]" : "text-primaryText hover:text-[#7E53F9] hover:-translate-x-1.5 hover:scale-125"}`}
+      />
+    </button>
   )
 }
 
