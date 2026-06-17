43b8ca80e8eac7c6015ae622ff9dba6a04f244c0
akmniazulalam <niazulalam097@gmail.com>
2026-06-18 00:29:29 +0600

    nested slider add

--- app/page.js
+++ app/page.js
@@ -1149,12 +1149,12 @@ export default function Home() {
                 </div>
               </Link>
             </div>
-            <div className="relative w-75! group">
-              {renderRecommendImageSlider()}
-              <div className="absolute top-5 right-5 h-7.5 w-7.5 bg-white rounded-full flex justify-center items-center group/wish hover:bg-hoverText transition-all duration-300 ease-[cubic-bezier(0.165, 0.84, 0.44, 1)] cursor-pointer">
-                <IoMdHeartEmpty className="text-primaryText group-hover/wish:text-white" />
-              </div>
-              <Link href={"/"}>
+            <div className="relative w-75! group flex! flex-col h-119.5">
+              <Link href={"/"} className="recoSlider flex flex-col h-119.5">
+                {renderRecommendImageSlider()}
+                <div className="flex-1 absolute top-5 right-5 h-7.5 w-7.5 bg-white rounded-full flex justify-center items-center group/wish hover:bg-hoverText transition-all duration-300 ease-[cubic-bezier(0.165, 0.84, 0.44, 1)] cursor-pointer">
+                  <IoMdHeartEmpty className="text-primaryText group-hover/wish:text-white" />
+                </div>
                 <div className="mt-2.5">
                   <h4 className="text-[18px] text-primaryText font-jost font-medium leading-7.5 w-[95%]">
                     <span
