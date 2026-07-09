630da623b5028b0d1e7c30e849687b184b7bafe5
akmniazulalam <niazulalam097@gmail.com>
2026-07-09 23:54:14 +0600

    feat: add initial splash screen for app loading

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,5 @@
-import { Geist, Geist_Mono, Jost } from "next/font/google";
 import "./globals.css";
+import { Geist, Geist_Mono, Jost } from "next/font/google";
 import Header from "./(components)/Header";
 import Footer from "./(components)/Footer";
 import SplashLoader from "./(components)/SplashLoader";
