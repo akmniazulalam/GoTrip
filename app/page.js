033d993bd2e30e3d4c320a500e5a3d039cad86c2
akmniazulalam <niazulalam097@gmail.com>
2026-07-09 23:50:03 +0600

    feat: add initial splash screen for app loading

--- app/page.js
+++ app/page.js
@@ -1,8 +1,8 @@
 "use client";
 import { React, useEffect, useRef, useState } from "react";
-import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 import Image from "next/image";
 import { RxArrowTopRight } from "react-icons/rx";
 import Container from "./(components)/Container";
