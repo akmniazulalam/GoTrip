d5a7f5cd1a150b7dca42cc3411b2a8f05dc33c10
akmniazulalam <niazulalam097@gmail.com>
2026-07-13 23:58:30 +0600

    unnecessary code remove

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 "use client";
-import { React, useEffect, useRef, useState } from "react";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { React, useEffect, useRef, useState } from "react";
 import Slider from "react-slick";
 import Image from "next/image";
 import { RxArrowTopRight } from "react-icons/rx";
